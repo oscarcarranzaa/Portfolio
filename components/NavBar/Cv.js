5aacfe10765d8bcf6915ab4079a7e51ec87ab0ba
oscar carranza <oscarcarranza504@gmail.com>
2022-04-14 14:51:49 -0600

    Top Header

--- components/NavBar/Cv.js
+++ components/NavBar/Cv.js
@@ -10,7 +10,7 @@ export function Cv () {
             <p>{t('DOWNLOAD_MY_RESUME')}</p>
             <div className={styles.Cv_link}>
                 <Link href="/" locale="en">
-                    <a>CV Hi</a>
+                    <a>CV hola</a>
                 </Link>
             </div>
         </div>
