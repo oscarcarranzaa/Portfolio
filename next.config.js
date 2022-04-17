4fd24d7c9ecde8023153d14032ee50341c4a6cc8
oscar carranza <oscarcarranza504@gmail.com>
2022-04-17 10:35:58 -0600

    Eslint fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  extends: [
+    'plugin:@next/next/recommended',
+    'plugin:react/recommended'
+  ],
   images: {
     domains: ['avatars.githubusercontent.com', 'i.ibb.co'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840]
