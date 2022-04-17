dcce8d372c238a7d63fb8610d9c49413b6eaca6a
oscar carranza <oscarcarranza504@gmail.com>
2022-04-17 14:53:36 -0600

    Responsive fix

--- components/Icons/Facebook.js
+++ components/Icons/Facebook.js
@@ -1,6 +1,6 @@
 export const Facebook = ({ color, background }) => {
   return (
-        <svg width="24" height="24" viewBox="0 0 32 32"><path fill={background} d="M0 0h32v32H0z"/><path fill={color} d="M22.08 32V19.607h4.16l.62-4.83h-4.78v-3.083c0-1.398.388-2.352 2.393-2.352h2.56V5.02c-.443-.058-1.962-.19-3.728-.19-3.688 0-6.213 2.25-6.213 6.385v3.562h-4.17v4.83h4.17V32h4.987z"/>
+        <svg width="30" height="30" viewBox="0 0 32 32"><path fill={background} d="M0 0h32v32H0z"/><path fill={color} d="M22.08 32V19.607h4.16l.62-4.83h-4.78v-3.083c0-1.398.388-2.352 2.393-2.352h2.56V5.02c-.443-.058-1.962-.19-3.728-.19-3.688 0-6.213 2.25-6.213 6.385v3.562h-4.17v4.83h4.17V32h4.987z"/>
     </svg>
   )
 }
