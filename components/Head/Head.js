a1099bc3a96eade00c5651a4dc27de839a8043d2
oscar carranza <oscarcarranza504@gmail.com>
2022-05-21 11:43:47 -0600

    google monetization added

--- components/Head/Head.js
+++ components/Head/Head.js
@@ -1,16 +1,24 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export function Headjs ({ title }) {
+export function Headjs({ title }) {
   return (
-        <Head>
-        <title>{title}</title>
-        <meta name="Author" content="Oscar Carranza" />
-        <meta name="keywords" content="Programador, desarrolladore web" />
-        <meta name="description" content="Bienvenido a mi portfolio donde podrás conocer un poco más de mí" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <Head>
+      <title>{title}</title>
+      <meta name="Author" content="Oscar Carranza" />
+      <meta name="keywords" content="Programador, desarrolladore web" />
+      <meta
+        name="description"
+        content="Bienvenido a mi portfolio donde podrás conocer un poco más de mí"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9647981077416807"
+        crossOrigin="anonymous"
+      ></script>
     </Head>
-  )
+  );
 }
 Headjs.defaultProps = {
-  title: 'Oscar Carranza'
-}
+  title: "Oscar Carranza",
+};
